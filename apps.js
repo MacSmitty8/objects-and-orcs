aae6d0525bc2324cd9c0ee3a84fabb31b824ede2
MacreenSmith <macreensmith@gmail.com>
2023-06-14 11:23:45 -0400

    create an object

---
+++ apps.js
@@ -0,0 +1,13 @@
+//Use an array inside an object
+
+const adventurer =  {
+    name: "Macreen",
+    hitPoint: 10,
+    belongings: ["sword", "potions", "bombs"
+]
+}
+
+//access the values in the array
+
+console.log(adventurer.belongings)
+console.log("hello world")
